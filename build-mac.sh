1d4fbceb53a2c72790ffbe75e3a689746e39deab
redturtlepower <redturtlepower@gmail.com>
2018-10-07 23:23:51 +0200

    corrected build spec in build-mac.sh and changed spec to x64

--- build-mac.sh
+++ build-mac.sh
@@ -1,5 +1,5 @@
 mkdir build
 cd build
-~/Qt5.11.2/5.11.2/clang_64/bin/qmake ./../helloworld.pro -spec=macx-clang CONFIG+=release
+~/Qt5.11.2/5.11.2/clang_64/bin/qmake ./../helloworld.pro -spec macx-clang CONFIG+=x86_64 CONFIG+=release
 make
 
