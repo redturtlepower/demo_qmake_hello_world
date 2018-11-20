81722c43fc136c049705cfa06211b67ff22736d1
thomas <thomas.rusche@gmail.com>
2018-11-21 00:56:11 +0100

    updated install path

--- build-ubuntu.sh
+++ build-ubuntu.sh
@@ -1,5 +1,5 @@
 mkdir build
 cd build
-~/Qt/5.11.2/gcc_64/bin/qmake ./../helloworld.pro -spec linux-gcc CONFIG+=x86_64 CONFIG+=release
+~/Qt5.11.2/5.11.2/gcc_64/bin/qmake ./../helloworld.pro -spec linux-gcc CONFIG+=x86_64 CONFIG+=release
 make
 
