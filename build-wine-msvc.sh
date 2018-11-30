03f85bc3cb6ade31498a9c2b9474bba5257d5e8e
thomas <thomas.rusche@gmail.com>
2018-11-30 22:44:54 +0100

    typos.

--- build-wine-msvc.sh
+++ build-wine-msvc.sh
@@ -6,7 +6,7 @@ GIT_LOG=`git log -1`
 # Author: John Doe <a@b.com>
 # Date: Sun Jun 3 00:35:34 2018 +0200
 echo 'git log -1:' $GIT_LOG
-COMMIT=$($GIT_LOG - 1 | cut -c7-47) # 'a39c68c4741639d4189b7ed4axd87b234b210797'
+COMMIT=$($git log - 1 | cut -c7-47) # 'a39c68c4741639d4189b7ed4axd87b234b210797'
 LINE_DATE=$(git log -1 | sed -n 3p)
 echo 'line date:' $LINE_DATE
 COMMIT_DATE=`echo $LINE_DATE | cut -c10-26` # 'Jun 3 00:35:34 2018 +0200'
