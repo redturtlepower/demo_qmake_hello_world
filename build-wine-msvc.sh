80a2e563b93c92629f13e38c5e092078c0a12aec
thomas <thomas.rusche@gmail.com>
2018-12-01 21:08:59 +0100

    use WINEPATH

--- build-wine-msvc.sh
+++ build-wine-msvc.sh
@@ -1,4 +1,4 @@
 mkdir build
 cd build
-wine ~/.wine/drive_c/buildenv/deps/qt-511/bin/qmake.exe ./../helloworld.pro -spec win32-msvc CONFIG+=x86_64 CONFIG+=release
+WINEPATH="/home/jenkins/.wine/drive_c/buildenv/deps/qt-511;/home/jenkins/.wine/drive_c/buildenv/deps/qt-511/bin;/home/jenkins/.wine/drive_c/buildenv/deps/qt-511/lib;/home/jenkins/.wine/drive_c/buildenv/deps/msvc14/bin;/home/jenkins/.wine/drive_c/buildenv/deps/msvc14/lib;/home/jenkins/.wine/drive_c/buildenv/deps/windows-kits/8.1/Include/shared;/home/jenkins/.wine/drive_c/buildenv/deps/windows-kits/8.1/Include/um;" wine qmake.exe ./../helloworld.pro -spec win32-msvc CONFIG+=x86_64 CONFIG+=release
 wine make
