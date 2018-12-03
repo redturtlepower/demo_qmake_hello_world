66bf15111690da9bce7e63f33334f87b67b69bf4
thomas <thomas.rusche@gmail.com>
2018-12-04 00:06:53 +0100

    different build folders for mac and ios

--- build-ios.sh
+++ build-ios.sh
@@ -1,5 +1,7 @@
-mkdir build
-cd build
+mkdir build-ios
+cd build-ios
 ~/Qt5.11.2/5.11.2/ios/bin/qmake ./../helloworld.pro -spec macx-ios-clang CONFIG+=x86_64 CONFIG+=release
 make
 
+xcodebuild -list -project helloworld.xcodeproj
+xcodebuild -scheme ${scheme} build
