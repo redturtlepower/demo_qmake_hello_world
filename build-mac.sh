3ecbbb618e583eefbd9a4addf4a9943f5d993393
thomas <thomas.rusche@gmail.com>
2018-12-04 00:44:50 +0100

    progress on xcode builds

--- build-mac.sh
+++ build-mac.sh
@@ -3,6 +3,7 @@ cd build-mac
 ~/Qt5.11.2/5.11.2/clang_64/bin/qmake ./../helloworld.pro -spec macx-clang CONFIG+=x86_64 CONFIG+=release
 make
 
-xcodebuild -list -project helloworld.xcodeproj
-scheme=helloworld
-xcodebuild -scheme ${scheme} build 
+# This actually seems not necessary to create helloworld.app on the mac...
+#xcodebuild -list -project helloworld.xcodeproj
+#scheme=helloworld
+#xcodebuild -scheme ${scheme} build 
